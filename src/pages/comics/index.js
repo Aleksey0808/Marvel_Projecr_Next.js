c9a3c29146bedc04cfb8c1be199e73ebc326397a
KonstantinYev <109310447+KonstantinYevchuk@users.noreply.github.com>
2023-08-25 17:07:02 +0300

    add Api for comics

--- src/pages/comics/index.js
+++ src/pages/comics/index.js
@@ -1,36 +1,39 @@
 import Head from "next/head";
 import Link from "next/link";
+// import { getAllComics } from "../../services/services";
 
-// export const getStaticProps = async () => {
-//     const response = await fetch('#');
-//     const data = await response.json();
+export const getStaticProps = async () => {
+    const response = await fetch('https://gateway.marvel.com:443/v1/public/comics?apikey=309dda4dc546766614178925997843a4');
+    // const data = await getAllComics();
+    const data = await response.json();
+    console.log(data);
 
-//     if (!data) {
-//       return {
-//         notFound: true
-//       };
-//     }
+    if (!data) {
+      return {
+        notFound: true
+      }
+    }
 
-//     return {
-//           props: {
-//             comics: data
-//           }
-//         };
-// }
+    return {
+          props: {
+            comics: data
+          },
+        };
+}
 
 const Comics = ({comics}) => {
-
+console.log(comics);
     return (
       <>
       <Head>
         <title>Comics</title>
       </Head>
       <ul>
-        {comics && comics.map(({id, title}) => (
+        {/* {comics && comics.map(({id, title}) => (
           <li key={id}>
             <Link href={`/comics/${id}`}>{title}</Link>
           </li>
-        ))}
+        ))} */}
         comics
       </ul>
       </>
--- src/services/services.js
+++ src/services/services.js
@@ -1,14 +1,25 @@
 import axios from "axios";
 
 const KEY = "309dda4dc546766614178925997843a4";
-axios.defaults.baseURL = "https://gateway.marvel.com:443";
+axios.defaults.baseURL = "https://gateway.marvel.com:443/v1/public/";
 
 // https://gateway.marvel.com:443/v1/public/characters?apikey=309dda4dc546766614178925997843a4
 
 
 export const getAllCharacters = async () => {
     try {
-        const response = await axios.get(`/v1/public/characters?apikey=${KEY}`)
+        const response = await axios.get(`characters?apikey=${KEY}`)
+        console.log(response);
+        return response
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const getAllComics = async () => {
+    try {
+        const response = await axios.get(`comics?apikey=${KEY}`)
         console.log(response);
         return response
         
