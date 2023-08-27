73630db1d0e98487cf4e0fdbf2c33e1664c602cf
Aleksey <103374981+Aleksey0808@users.noreply.github.com>
2023-08-27 20:55:06 +0300

    add bg image and logo ico

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,7 @@ import Layout from "@/components/Layout";
 const App = ({ Component, pageProps }) => (
    <Layout>
       <Head>
+      <link rel="icon" href="../../public/favicon.ico" />
       <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap" rel="stylesheet" />
       </Head>
       <main>
