3982271d19278d0ea1b288e9174a0fdce83fe5c7
Aleksey0808 <103374981+Aleksey0808@users.noreply.github.com>
2023-08-29 21:29:59 +0300

    add images for types pokemon

--- src/components/TypesInfo.js
+++ src/components/TypesInfo.js
@@ -1,25 +1,39 @@
-import Link from "next/link";
-import styles from "../styles/TypesInfo.module.scss";
-import { useRouter } from 'next/navigation';
+import Link from 'next/link'
+import styles from '../styles/TypesInfo.module.scss'
+import { useRouter } from 'next/navigation'
 
-const TypesInfo = ({type}) => {
-    // console.log(type);
-    const router = useRouter();
+const TypesInfo = ({ type }) => {
+  // console.log(type);
+  const router = useRouter()
 
-    return (
-        <>
-        <button className={styles.btn} type="button" onClick={() => router.back()}> &#8592; Go back</button>   
-        {/* <div className={styles.container}> */}
-            <ul className={styles.list}>
-            {type && type.map(({pokemon}) => {
-                console.log(pokemon)
-                return(
-                    <Link className={styles.link} href={`/pokemons/${pokemon.name}`}><li key={pokemon.name}>{pokemon.name}</li></Link>
-                    )
-            })}
-            </ul>
-        </>
-    )
-};
+  return (
+    <>
+      <button
+        className={styles.btn}
+        type="button"
+        onClick={() => router.back()}
+      >
+        {' '}
+        &#8592; Go back
+      </button>
+      {/* <div className={styles.container}> */}
+      <ul className={styles.list}>
+        {type &&
+          type.map(({ pokemon }) => {
+            // console.log(pokemon)
+            return (
+              <Link
+                key={pokemon.name}
+                className={styles.link}
+                href={`/pokemons/${pokemon.name}`}
+              >
+                <li key={pokemon.name}>{pokemon.name}</li>
+              </Link>
+            )
+          })}
+      </ul>
+    </>
+  )
+}
 
-export default TypesInfo;
+export default TypesInfo
--- src/pages/types/index.js
+++ src/pages/types/index.js
@@ -1,53 +1,69 @@
-import Head from "next/head";
-import Link from "next/link";
+import Head from 'next/head'
+import Link from 'next/link'
 import styles from '../../styles//Types.module.scss'
-
+import icon from '../../../public/iconTypes.json'
+import Image from 'next/image'
 
 export const getStaticProps = async () => {
-    const response = await fetch('https://pokeapi.co/api/v2/type/');
-    const data = await response.json();
+  const response = await fetch('https://pokeapi.co/api/v2/type/')
+  const data = await response.json()
 
-    if (!data) {
-      return {
-        notFound: true
-      }
-    }
-
+  if (!data) {
     return {
-          props: {
-            types: data
-          },
-        };
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      types: data,
+    },
+  }
 }
 
-const Types = ({types}) => {
+const Types = ({ types }) => {
   // console.log(types);
-const { results } = types;
-    return (
-      <>
+  const { results } = types
+  return (
+    <>
       <Head>
         <title>Types</title>
       </Head>
-      
+
       <ul>
-        {results && results.map(({name}) => {
+        {results &&
+          results.map(({ name }) => {
+            let str = name
+            const newName = (str) => str.charAt(0).toUpperCase() + str.slice(1)
+            // console.log(str)
+            // console.log(newName(str) === icon.src)
 
-          let str = name;
-          const newName = str => str.charAt(0).toUpperCase() + str.slice(1);
-          console.log(str)
-          console.log(newName(str))
-
-          return (
-            <li key={name} className={styles.list}>
-            {newName(str)}  
-            <Link className={styles.link} href={`/types/${name}`}><button className={styles.btn}>Info</button></Link>
-          </li>
-          )
-        })}
+            return (
+              <li key={name} className={styles.list}>
+                {newName(str)}
+                {icon.map(({ src, name }) => {
+                  // console.log(src)
+                  if (name === newName(str)) {
+                    return (
+                      <Image
+                        key={src}
+                        src={src}
+                        width={150}
+                        height={150}
+                        alt={name}
+                      />
+                    )
+                  }
+                })}
+                <Link className={styles.link} href={`/types/${name}`}>
+                  <button className={styles.btn}>Info</button>
+                </Link>
+              </li>
+            )
+          })}
       </ul>
-      </>
-    );
-};
+    </>
+  )
+}
 
-export default Types;
-
+export default Types
